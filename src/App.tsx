95cc7ad667821594d9ebd5d6476385181d4d8c44
Taufiq Zarif <taufiqzarif90@gmail.com>
2025-11-19 22:00:46 +0800

    Implement login page with input fields and button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import Login from "./pages/Login";
+
 const App = () => {
-	return <div>App</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/login" element={<Login />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
